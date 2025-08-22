835c26bcf21ff1d2f9c13d81f4a2993bdee9149d
Shraddha7001 <yshraddha729@gmail.com>
2025-08-22 12:05:24 +0530

    Basic operations in java

---
+++ Operators.java
@@ -0,0 +1,17 @@
+public class Operators {
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+
+        int num3=30;
+        System.out.println(num1+num2+num3);
+        System.out.println(num1-num3);
+        System.out.println(num1*num2*num3);
+        System.out.println(num3/num2);
+        System.out.println(num3%num2);
+        
+
+
+    }
+    
+}
