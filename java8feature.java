0c4e5207ebbed6630d75d7d16e37addb2cabf567
shraddhayadav <161943519+Shraddha7001@users.noreply.github.com>
2025-09-05 17:40:58 +0530

    Add files via upload
    Java 8 feature in interface

---
+++ java8feature.java
@@ -0,0 +1,33 @@
+interface A{
+    void show();
+    default void config()
+    {
+        System.out.println("In config");
+    }
+    static void place()
+    {
+        System.out.println("In place");
+    }
+}
+
+
+class B implements A{
+    public void show()
+    {
+        System.out.println("in show");
+    }
+}
+
+
+
+
+
+
+public class java8feature {
+    public static void main(String[] args) {
+        A.place();
+        A obj=new B();
+        obj.show();
+        obj.config();
+    }
+}
