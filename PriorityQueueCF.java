d367be7d3a363ec1a01eae5d543b80f49b9ed108
shraddhayadav <161943519+Shraddha7001@users.noreply.github.com>
2025-09-10 13:58:32 +0530

    Priority Queue

---
+++ PriorityQueueCF.java
@@ -0,0 +1,34 @@
+import java.util.PriorityQueue;
+
+public class PriorityQueueCF {
+    public static void main(String[] args) {
+        
+        PriorityQueue pq=new PriorityQueue();
+
+
+        pq.add("billuda");
+        pq.add("kittuda");
+        pq.add("mewo_mewo");
+        pq.add("bho_bho_bho");
+        pq.add("panduu");
+        pq.add("jerru");
+
+        System.out.println(pq);
+
+
+        PriorityQueue pq2=new PriorityQueue();
+
+        pq2.add(7);
+        pq2.add(7);
+        pq2.add(9);
+        pq2.add(11);
+        pq2.add(6);
+        pq2.add(5);
+
+
+
+        System.out.println(pq2);
+    }
+
+    
+}
