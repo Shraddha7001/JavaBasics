23a1f16b860a72ecd098912e9058e6f95036b720
shraddhayadav <161943519+Shraddha7001@users.noreply.github.com>
2025-09-15 22:59:42 +0530

    Swapping of elements of an array

---
+++ SwappingOfArray.java
@@ -0,0 +1,29 @@
+//time complexity=O(n)
+//space cpmplexity=O(1)
+
+
+
+public class SwappingOfArray {
+    public static void main(String[] args) {
+        
+
+        int arr[]={2,4,6,8,10,12};
+        int n=arr.length;
+
+
+        for(int i=0;i<n/2;i++){
+            int temp=arr[i];
+            arr[i]=arr[n-i-1];
+            arr[n-i-1]=temp;
+        }
+
+        System.out.println("Reversal of an array is :: ");
+
+
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        // System.out.println();
+    }
+}
